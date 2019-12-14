cc1e31887e531782085a80a6dc7787904558a855
Tim <0xtimc@gmail.com>
2019-12-14 00:28:29 +0000

    Make tags unique

--- Sources/SteampressFluentPostgres/Models/BlogTag+Fluent.swift
+++ Sources/SteampressFluentPostgres/Models/BlogTag+Fluent.swift
@@ -7,4 +7,12 @@ extension BlogTag: Model {
     public static var idKey: IDKey { return \.tagID }
 }
 
-extension BlogTag: Migration {}
+extension BlogTag: Migration {
+    public static func prepare(on connection: PostgreSQLConnection) -> EventLoopFuture<Void> {
+        return Database.create(BlogTag.self, on: connection) { builder in
+            builder.field(for: \.tagID, isIdentifier: true)
+            builder.field(for: \.name)
+            builder.unique(on: \.name)
+        }
+    }
+}
